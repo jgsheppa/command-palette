5671e20e9cbb84a520df73c36127ba97c3ffa887
jgsheppa <james.sheppard@avisio.ai>
2023-03-21 23:58:48 +0100

    add search input and improve keyboard mobility

--- components/CommandPalette.tsx
+++ components/CommandPalette.tsx
@@ -1,80 +1,119 @@
-import { ReactNode, useMemo } from 'react';
+import { ChangeEvent, ReactNode, useMemo, useState } from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 
 import { useCommandPalette } from '@/hooks/useCommandPalette';
 
-const routes = [{ path: '/about', pageName: "About" }, { path: '/', pageName: "Home" }, { path: '/contact', pageName: "Contact" }, { path: '/security', pageName: "Security" }, { path: '/blog', pageName: "Blog" }];
+const routes = [
+  { path: '/about', content: 'About' },
+  { path: '/', content: 'Home' },
+  { path: '/contact', content: 'Contact' },
+  { path: '/security', content: 'Security' },
+  { path: '/blog', content: 'Blog' },
+];
 
 export function CommandPalette() {
-  const { isCommandPaletteOpen, handleClosePalette } = useCommandPalette(routes.length);
-  const router = useRouter();
 
-  const otherRoutes = useMemo(
-    () => routes.filter((route) => route.path !== router.route),
-    [router.route],
+  const { isCommandPaletteOpen, handleClosePalette } = useCommandPalette(
+    routes.length,
   );
 
+  const [commandData, setCommandData] = useState(routes)
+
   return isCommandPaletteOpen ? (
-    <CommandModal>
-      <ul>
-        {otherRoutes.map((route, index) => (
-          <li key={index}>
-            <Link href={route.path} onClick={handleClosePalette}>
-              <div id={`route-${index + 1}`} tabIndex={index + 1} className="relative p-4">
-                <p>{route.pageName}</p>
-              </div>
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <CommandModal
+      data={routes}
+      setCommandData={setCommandData}
+      handleClosePalette={handleClosePalette}
+    >
+      {commandData.map((route, index) => (
+        <Link key={index} href={route.path} onClick={handleClosePalette} tabIndex={0}>
+          <div
+            id={`command-${index + 1}`}
+            className="relative p-4"
+          >
+            <p>{route.content}</p>
+          </div>
+        </Link>
+      ))}
     </CommandModal>
   ) : null;
 }
 
 type CommandModalProps = {
   children: ReactNode;
-}
+  data: { path: string; content: string }[];
+  setCommandData: (data: { path: string; content: string }[]) => void;
+  handleClosePalette: () => void;
+};
 
-function CommandModal({ children }: CommandModalProps) {
-  const { handleClosePalette } = useCommandPalette(routes.length);
+function CommandModal({
+  children,
+  data,
+  setCommandData,
+  handleClosePalette,
+}: CommandModalProps) {
+  const [searchInput, setSearchInput] = useState('');
 
-  return <div
-    data-te-modal-init
-    className="fixed top-0 left-0 z-[1055] block h-full w-full overflow-y-auto overflow-x-hidden outline-none"
-    id="exampleModalCenter"
-    tabIndex={-1}
-    aria-labelledby="exampleModalCenterTitle"
-    aria-modal="true"
-    role="dialog">
+  const handleSearchInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.target.value);
+    setCommandData(data.filter(command => command.content.includes(e.target.value)))
+  };
+
+  return (
     <div
-      data-te-modal-dialog-ref
-      className="pointer-events-none relative flex min-h-[calc(100%-1rem)] w-auto translate-y-[-50px] items-center transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:min-h-[calc(100%-3.5rem)] min-[576px]:max-w-[500px]">
+      data-te-modal-init
+      className="fixed top-0 left-0 z-[1055] block h-full w-full overflow-y-auto overflow-x-hidden outline-none"
+      id="exampleModalCenter"
+      tabIndex={-1}
+      aria-labelledby="exampleModalCenterTitle"
+      aria-modal="true"
+      role="dialog"
+    >
       <div
-        className="pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600">
-        <div
-          className="flex flex-shrink-0 items-center justify-end rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
-          <button
-            type="button"
-            className="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
-            data-te-modal-dismiss
-            aria-label="Close" onClick={handleClosePalette}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="h-6 w-6">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </button>
+        data-te-modal-dialog-ref
+        className="pointer-events-none relative flex min-h-[calc(100%-1rem)] w-auto translate-y-[-50px] items-center transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:min-h-[calc(100%-3.5rem)] min-[576px]:max-w-[500px]"
+      >
+        <div className="pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600">
+          <div className="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
+            <div className="relative p-4">
+              <input
+                className="block"
+                placeholder="Search Commands"
+                data-te-modal-search
+                aria-label="command search"
+                id="command-0"
+                tabIndex={0}
+                onChange={handleSearchInput}
+                value={searchInput}
+              />
+            </div>
+            <button
+              tabIndex={0}
+              type="button"
+              className="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none focus:outline-blue-600"
+              data-te-modal-dismiss
+              aria-label="Close"
+              onClick={handleClosePalette}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+          {children}
         </div>
-        {children}
       </div>
     </div>
-  </div>
+  );
 }
--- hooks/useCommandPalette.ts
+++ hooks/useCommandPalette.ts
@@ -1,58 +1,55 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from 'react';
 
 export function useCommandPalette(dataLength: number) {
-  const ref = useRef(0)
-  const [isCommandPaletteOpen, setIsCommandPaletteOpen] = useState(false)
-  
+  const ref = useRef(0);
+  const [isCommandPaletteOpen, setIsCommandPaletteOpen] = useState(false);
+
   useEffect(() => {
     const onKeydown = (e: KeyboardEvent) => {
-      if (e.key === "k" && e.ctrlKey) {
+      if (e.key === 'k' && e.ctrlKey) {
         e.preventDefault();
         setIsCommandPaletteOpen(true);
-        ref.current = 0
-      } else if (e.code === "Escape") {
+      } else if (e.code === 'Escape') {
         e.preventDefault();
         setIsCommandPaletteOpen(false);
       }
     };
-    window.addEventListener("keydown", onKeydown);
+    window.addEventListener('keydown', onKeydown);
     return () => {
-      window.removeEventListener("keydown", onKeydown);
+      window.removeEventListener('keydown', onKeydown);
     };
   }, [setIsCommandPaletteOpen]);
-  
+
   useEffect(() => {
     const onArrowdown = (e: KeyboardEvent) => {
-      if (isCommandPaletteOpen && e.key === "ArrowDown") {
+      if (isCommandPaletteOpen && e.key === 'ArrowDown') {
         e.preventDefault();
-        if (ref.current === dataLength - 1) {
-        ref.current = 1
+        if (ref.current === dataLength) {
+          ref.current = 0;
         } else {
-          ref.current = ref.current + 1
+          ref.current = ref.current + 1;
         }
-        document.getElementById(`route-${ref.current}`)?.focus()
-
-      } else if (isCommandPaletteOpen && e.key === "ArrowUp") {
+        document.getElementById(`command-${ref.current}`)?.focus();
+      } else if (isCommandPaletteOpen && e.key === 'ArrowUp') {
         e.preventDefault();
-        if (ref.current === 1) {
-          ref.current = dataLength - 1
+        if (ref.current === 0) {
+          ref.current = dataLength;
         } else {
-          ref.current = ref.current - 1
+          ref.current = ref.current - 1;
         }
-        document.getElementById(`route-${ref.current}`)?.focus()
-      } 
+        document.getElementById(`command-${ref.current}`)?.focus();
+      }
     };
-    console.log("🚀 ~ file: useCommandPalette.ts:40 ~ onArrowdown ~ ref:", ref)
-    window.addEventListener("keydown", onArrowdown);
+    window.addEventListener('keydown', onArrowdown);
     return () => {
-      window.removeEventListener("keydown", onArrowdown);
+      window.removeEventListener('keydown', onArrowdown);
     };
   }, [dataLength, isCommandPaletteOpen, setIsCommandPaletteOpen]);
 
   const handleClosePalette = () => {
-    setIsCommandPaletteOpen(false)
-    ref.current = 0
-  }
+    setIsCommandPaletteOpen(false);
+    ref.current = 0;
+  };
 
-  return { isCommandPaletteOpen, handleClosePalette }
+  return { isCommandPaletteOpen, handleClosePalette };
 }
